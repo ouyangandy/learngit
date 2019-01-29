f647b8feda0c9e4d0678f7e478e7bb0f2f36c787
ouyangandy <17688758962@163.com>
2019-01-29 10:49:35 +0800

    add a class

--- a.py
+++ a.py
@@ -1,3 +1,11 @@
 #author ouminghua
 print("hello world")
 print("go home!")
+class ouminghua(object):
+	"""
+	docstring for ouminghua
+	"""
+	def __init__(self, arg):
+		super(ouminghua
+		, self).__init__()
+		self.arg = arg
